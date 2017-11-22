7871ac4264e1be64ab8bb0ed4546d32baca7dc1b
tshi206 <tshi206@aucklanduni.ac.nz>
2017-11-22 22:04:53 +1300

    added mapped js

---
+++ dist/stuff.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var counter = function counter(arr) {
+    return "there are " + arr.length + " elements in this array";
+};
+
+var adder = function adder(a, b) {
+    return a + b;
+};
+
+var pi = 3.1415926;
+
+//console.log(counter(["cate", "mike", "james"]));
+
+module.exports = {
+    counter: counter,
+    adder: adder,
+    pi: pi
+};
+//# sourceMappingURL=stuff.js.map
